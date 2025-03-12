cdd88de38e3c6a58188ddfb92e798e32dea1cb40
Camila Albuquerque <camila.albuquerque15@gmail.com>
2025-03-12 20:22:56 -0300

    Excluídos imports não utilizados.

--- src/test/java/br/edu/ifgoias/academico/services/CursoServiceTest.java
+++ src/test/java/br/edu/ifgoias/academico/services/CursoServiceTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
